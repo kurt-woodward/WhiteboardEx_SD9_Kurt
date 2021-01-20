4b81c21bede34fb979eb8c0441f70267df5e77a2
Kurt Woodward <kurt.woodward@outlook.com>
2021-01-20 09:37:51 -0800

    New repository for MSSA CAD SD9

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WhiteboardEx_SD9_Kurt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whiteboard Solutions Container.");
+        }
+
+    }
+}
