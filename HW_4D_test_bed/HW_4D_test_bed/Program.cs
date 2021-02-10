95433f26f6ff8716e97beeb8f032c4a810e0fa44
Kurt Woodward <kurt.woodward@outlook.com>
2021-02-10 08:18:37 -0800

    Figuring out how to Push code to GitHub...

---
+++ HW_4D_test_bed/HW_4D_test_bed/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW_4D_test_bed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Multi(int x)
+            {
+                if (x > 0)
+                {
+                    return (x * x);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
---
+++ PalindromeTest/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+// using System.Linq;
+namespace PalindromeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "";
+
+            string s1 = "dog";
+            string s2 = "cat";
+            string s3 = "tacocat";
+            string s4 = "hotdog";
+            string s5 = "amanaplanacanalpanama";
+
+
+
+            isPalindrome2(s1);
+            isPalindrome2(s2);
+            isPalindrome2(s3);
+            isPalindrome2(s4);
+            isPalindrome2(s5);
+        }
+
+        //private static bool isPalindrome2(string testString)
+        {
+            string rvrs = "";
+            for (int i = testString.Length; i <= 0 ; i--)
+            {
+                rvrs += testString[i];
+            }
+
+            Console.WriteLine($"Palindrome test {testString} <> {isPalindrome(testString)}");
+            return true;
+        }
+
+        private static bool isPalindrome(string testString)
+        {
+            string palindrome = testString;
+            for (int i = 0; i < testString.Length/2; i++)
+            {
+                for (int j = testString.Length; j > 0; j--)
+                {
+                    int last = testString.Length - 1;
+                    if (testString[i] != testString[last - i]) return false;
+                }
+
+            }
+            return true;
+            //// string palindrome = (string)testString.Reverse();
+            //if (testString == palindrome)
+            //{
+            //Console.WriteLine($"{testString} is a palindrome!");
+            //    return true;
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{testString} is NOT a palindrome!");
+            //    return false;
+            //}
+        }
+    }
+}
